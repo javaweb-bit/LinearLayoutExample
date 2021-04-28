722df1b514d37e8fa8c7228ef4b3e07f5c70918d
남승균 <skyun.nam@gmail.com>
2021-04-28 15:28:25 +0900

    Initial Commit

---
+++ app/src/main/java/com/example/linearlayoutexample/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.linearlayoutexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View v) { }
+    public void onButton2Clicked(View v) { }
+    public void onButton3Clicked(View v) { }
+    public void onButton4Clicked(View v) { }
+    public void onButton5Clicked(View v) { }
+    public void onButton6Clicked(View v) { }
+    public void onButton7Clicked(View v) { }
+    public void onButton8Clicked(View v) { }
+    public void onButton9Clicked(View v) { }
+
+}
